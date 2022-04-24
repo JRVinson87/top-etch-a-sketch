fb7c17b304cc4332aa9769e4fc1b3d6d4c5987a1
Joshua Vinson <JRVinson87@gmail.com>
2022-04-24 17:28:56 -0600

    create 16x16 box divs

---
+++ etch-a-sketch.js
@@ -0,0 +1,15 @@
+function createEtchWindow() {
+    const container = document.querySelector("#etch-container");
+    
+
+    // set up a 16 by 16 etch window
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const div = document.createElement("div");
+            div.classList.add('etch-block-init');
+            container.appendChild(div);
+        }
+    }
+}
+
+createEtchWindow();
