1c358caf340bc30c2c0fbad0bb10156994e4fa27
Joshua Vinson <JRVinson87@gmail.com>
2022-04-24 17:59:18 -0600

    add css ids for init block and color block

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,18 @@
 function createEtchWindow() {
     const container = document.querySelector("#etch-container");
-    
 
     // set up a 16 by 16 etch window
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             const div = document.createElement("div");
-            div.classList.add('etch-block-init');
+            div.classList.add('etch-block');
             container.appendChild(div);
         }
     }
+}
+
+function addBlockListener() {
+    
 }
 
 createEtchWindow();
