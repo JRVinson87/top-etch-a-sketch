ef83db4db057357afad7446211c33baebbbab829
Joshua Vinson <JRVinson87@gmail.com>
2022-04-24 20:46:23 -0600

    added a way to turn on and off ink on mouse hover with a click

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,13 +6,38 @@ function createEtchWindow() {
         for (let j = 0; j < 16; j++) {
             const div = document.createElement("div");
             div.classList.add('etch-block');
+            div.setAttribute('draggable', 'false');
             container.appendChild(div);
         }
     }
 }
 
-function addBlockListener() {
-    
+function colorBlock(e) {
+    e.target.style = 'background-color: black';
 }
 
+function sketchListener() {
+    const block = document.querySelectorAll("div.etch-block");
+    
+    if (!inkActive) {
+        console.log(inkActive);
+        block.forEach(div => {
+            div.addEventListener('mouseenter', colorBlock);
+        })
+        inkActive = true;
+    } else { 
+        console.log(inkActive);
+        block.forEach(div => {
+            div.removeEventListener('mouseenter', colorBlock); 
+    })
+        inkActive = false;
+    }
+}
+
+const container = document.querySelector("#etch-container");
+let inkActive = false;
+
 createEtchWindow();
+
+
+container.addEventListener('click', sketchListener);
