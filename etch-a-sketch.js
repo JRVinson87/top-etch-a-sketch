af6212c12a92a6cd83daa6efe01f7b970f21a339
Joshua Vinson <JRVinson87@gmail.com>
2022-04-24 22:21:07 -0600

    added slider to select grid input size and button to create new grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,20 @@
-function createEtchWindow(blocks) {
+function createEtchWindow() {
+    delOldGrid();
 
-    container.style['grid-template-columns'] = `repeat(${blocks}, 1fr)`;
-    container.style['grid-template-rows'] = `repeat(${blocks}, 1fr)`;
+    container.style['grid-template-columns'] = `repeat(${gridSize}, 1fr)`;
+    container.style['grid-template-rows'] = `repeat(${gridSize}, 1fr)`;
 
-    for (let i = 0; i < Math.pow(blocks, 2); i++) {
+    for (let i = 0; i < Math.pow(gridSize, 2); i++) {
         const div = document.createElement("div");
         div.classList.add('etch-block');
         div.setAttribute('draggable', 'false');
         container.appendChild(div);
+    }
+}
+
+function delOldGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
     }
 }
 
@@ -31,10 +38,23 @@ function sketchListener() {
     }
 }
 
-const container = document.querySelector("#etch-container");
+const container = document.getElementById('etch-container');
+const newGridBtn = document.getElementById('btn-new-grid')
+
 let inkActive = false;
+let slider = document.getElementById('grid-slider');
+let sliderText = document.getElementById('slider-label');
+let gridSize = 16;
 
-createEtchWindow(10);
+sliderText.textContent = slider.value;
 
+slider.oninput = function() {
+    sliderText.textContent = this.value;
+    gridSize = this.value;  
+}
+
+createEtchWindow();
+
+newGridBtn.addEventListener('click', createEtchWindow);
 
 container.addEventListener('click', sketchListener);
