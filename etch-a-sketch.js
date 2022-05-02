e2f31f4f591f0096e4898a2bf772408ded7acccd
Joshua Vinson <JRVinson87@gmail.com>
2022-05-01 20:35:24 -0600

    Added btn to change block color between black and random RGB. Added color change functionality.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -18,8 +18,27 @@ function delOldGrid() {
     }
 }
 
+function rgbToggle() {
+    if (!rgbActive) {
+        rgbActive = true;
+        rgbToggleBtn.textContent = "Change Color to: Black";
+    } else {
+        rgbActive = false;
+        rgbToggleBtn.textContent = "Change Color to: CHAOS RGB";
+    }
+}
+
 function colorBlock(e) {
-    e.target.style = 'background-color: black';
+    if(rgbActive === true) {
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+  
+        e.target.style = "background-color: rgb(" + x + "," + y + "," + z + ")";
+
+    } else {
+        e.target.style = 'background-color: black';
+    }
 }
 
 function sketchListener() {
@@ -39,9 +58,11 @@ function sketchListener() {
 }
 
 const container = document.getElementById('etch-container');
-const newGridBtn = document.getElementById('btn-new-grid')
+const newGridBtn = document.getElementById('btn-new-grid');
+const rgbToggleBtn = document.getElementById('btn-rgb-toggle');
 
 let inkActive = false;
+let rgbActive = false;
 let slider = document.getElementById('grid-slider');
 let sliderText = document.getElementById('slider-label');
 let gridSize = 16;
@@ -56,5 +77,6 @@ slider.oninput = function() {
 createEtchWindow();
 
 newGridBtn.addEventListener('click', createEtchWindow);
+rgbToggleBtn.addEventListener('click', rgbToggle);
 
 container.addEventListener('click', sketchListener);
